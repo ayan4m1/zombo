4a5bf85c2869b729022669a0c9f12ca92142de7d
ayan4m1 <andrew@bulletlogic.com>
2012-04-09 05:13:33 -0400

    added ZomboDropInfo structure to wrap mob drops

---
+++ src/main/java/me/ayan4m1/plugins/zombo/ZomboDropInfo.java
@@ -0,0 +1,39 @@
+package me.ayan4m1.plugins.zombo;
+
+import java.util.Random;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+public class ZomboDropInfo extends ItemStack {
+	private Float chance;
+	private Random rand;
+
+	public ZomboDropInfo(Material type, Float chance) {
+		super(type, 1);
+		this.chance = chance;
+		this.rand = new Random();
+	}
+
+	public ZomboDropInfo(Material type, Float chance, Integer amount) {
+		super(type, amount);
+		this.chance = chance;
+		this.rand = new Random();
+	}
+
+	public Float getChance() {
+		return chance;
+	}
+
+	public void setChance(Float chance) {
+		this.chance = chance;
+	}
+
+	public void setRandomSeed(long seed) {
+		this.rand.setSeed(seed);
+	}
+
+	public boolean evaluate() {
+		return (rand.nextFloat() < chance);
+	}
+}
