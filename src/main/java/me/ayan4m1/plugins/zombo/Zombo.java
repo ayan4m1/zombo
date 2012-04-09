49b1e41738971e2157ebabb6cce3b82de499353e
ayan4m1 <andrew@bulletlogic.com>
2012-04-08 22:56:32 -0400

    added skeleton command handler and command declarations in plugin.yml

--- src/main/java/me/ayan4m1/plugins/zombo/Zombo.java
+++ src/main/java/me/ayan4m1/plugins/zombo/Zombo.java
@@ -1,5 +1,8 @@
 package me.ayan4m1.plugins.zombo;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -17,6 +20,13 @@ public class Zombo extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         event.getPlayer().sendMessage("Welcome, " + event.getPlayer().getDisplayName() + "!");
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String cmdLabel, String[] args) {
+    	if (!(sender instanceof Player)) {
+    		return false;
+    	}
+    	return false;
     }
 }
 
